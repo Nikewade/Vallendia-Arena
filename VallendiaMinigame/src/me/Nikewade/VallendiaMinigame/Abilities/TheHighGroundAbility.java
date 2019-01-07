c7b309b6f5c6981acbc4073d67d60c752d8c28f2
Nikewade <nikewade1006@gmail.com>
2019-01-07 11:36:07 +0000

    added some abilities

---
+++ VallendiaMinigame/src/me/Nikewade/VallendiaMinigame/Abilities/TheHighGroundAbility.java
@@ -0,0 +1,107 @@
+package me.Nikewade.VallendiaMinigame.Abilities;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+import me.Nikewade.VallendiaMinigame.VallendiaMinigame;
+import me.Nikewade.VallendiaMinigame.Interface.Ability;
+
+public class TheHighGroundAbility implements Ability, Listener{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "The High Ground";
+	}
+
+	@Override
+	public AbilityType getAbilityType() {
+		// TODO Auto-generated method stub
+		return AbilityType.PASSIVE;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return "When higher than your enemy, deal more and take less damage";
+	}
+
+	@Override
+	public ItemStack getGuiItem() {
+		// TODO Auto-generated method stub
+		return new ItemStack(Material.SMOOTH_STAIRS);
+	}
+
+	@Override
+	public boolean RunAbility(Player p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+    public static Listener getListener() {
+        return new Listener() {
+        	
+        	
+        	
+        	
+            	@EventHandler
+            	public void onDamage(EntityDamageByEntityEvent e)
+            	{
+            		double lowerPercent =  0.8 ; //20%
+            		double higherPercent =  1.2 ; //20%
+            		double damage = e.getDamage();
+            		double lowerDamage = damage * lowerPercent;
+            		double higherDamage = damage * higherPercent;
+
+            		if(e.getDamager() instanceof Player && e.getEntity() instanceof LivingEntity)
+            		{
+            			Player damager = (Player) e.getDamager();
+            			LivingEntity target = (LivingEntity) e.getEntity();
+            			
+                		//person attacking has the ability
+                		if(VallendiaMinigame.getInstance().abilitymanager.playerHasAbility(damager, "The High Ground"))
+                		{
+                			if(damager.getLocation().getY() > target.getLocation().getY() && damager.isOnGround())
+                			{
+                				e.setDamage(higherDamage);
+                	 	 		target.getWorld().playSound(target.getLocation(), Sound.ENTITY_PLAYER_ATTACK_CRIT, 1, 1);
+                	 	 		target.getWorld().spawnParticle(Particle.CRIT, target.getLocation().add(0, 1.8, 0), 20);
+                			}
+                		}
+                		
+                		
+            			//person getting damaged has the ability
+                		if(target instanceof Player)
+                		{
+                			Player targetP = (Player) target;
+                    		if(VallendiaMinigame.getInstance().abilitymanager.playerHasAbility(targetP, "The High Ground"))
+                    		{
+                    			if(targetP.getLocation().getY() > damager.getLocation().getY() && targetP.isOnGround())
+                    			{
+                    				e.setDamage(lowerDamage);	
+                    	 	 		damager.getWorld().playSound(target.getLocation(), Sound.ITEM_SHIELD_BLOCK, 1, 1);
+                    			}
+                    		}
+                		}
+            		}
+            	}
+
+            
+            
+            
+            
+            
+            
+        };
+    }
+
+}
